180e14b847526334e4fac970ceddaa392c5e029d
Alison Chen <alisonchen.y@gmail.com>
2023-10-30 11:54:03 -0700

    add and replace courses from spreadsheet

---
+++ 01_cleaning_raw_data/03_update_course.R
@@ -0,0 +1,30 @@
+# add/replace courses after adding school column
+# add course from spreadsheet
+library(readxl)
+library(dplyr)
+courses <- read.csv("usc_courses_updated_with_school.csv")
+courses_to_update <- read_excel("Courses_to_add_to_SOC_20241.xlsx")
+
+courses_to_add <- courses_to_update %>% filter(Data_Instructions == "Add")
+courses_to_add$department <- sapply(courses_to_add$courseID, function(x) {
+  strsplit(x, "-")[[1]][1]
+})
+
+courses_to_replace <- courses_to_update %>% filter(Data_Instructions == "Replace")
+courses_new <- rbind(courses, courses_to_add[,names(courses)])
+for (i in 1:nrow(courses_to_replace)) {
+  j <- which(courses_new$courseID == courses_to_replace$courseID[i] &
+               courses_new$semester == courses_to_replace$semester[i])
+  courses_new[j,] <- courses_to_replace[i,names(courses_new)]
+  courses_new[j,]$department <- strsplit(courses_new[j,]$courseID, "-")[[1]][1]
+}
+
+# arrange semesters in order then create all_semesters column
+courses_new <- courses_new %>%
+  group_by(courseID) %>%
+  mutate(all_semesters = paste(unique(semester), collapse = ", ")) %>%
+  ungroup()
+
+write.csv(courses_new,
+          "usc_courses_updated_with_school_updated.csv",
+          row.names = FALSE)
---
+++ 01_cleaning_raw_data/04_cleaning_course_descriptions.R
@@ -0,0 +1,33 @@
+# cleaning the course descriptions for mapping
+
+library(tidyverse)
+library(stringr)
+library(stringi)
+
+usc_courses = read.csv("usc_courses_updated_with_school_updated.csv")
+# usc_courses = read.csv("usc_courses.csv")
+
+# context dependency
+# replace certain phrases with new phrases
+apply_context_dependency <- function(tt) {
+  tt <- tolower(tt)
+  corrections <- read.csv("Data/context_dependencies_09_20_23.csv")
+  corrections$before <- tolower(corrections$before)
+  corrections$after <- tolower(corrections$after)
+  tt <- stri_replace_all_regex(tt,
+                               pattern = corrections$before,
+                               replacement = corrections$after,
+                               vectorize = FALSE)
+  tt
+}
+
+# remove all punctuation except '
+remove_punctuation <- function(tt) {
+  gsub("[^[:alnum:][:space:]']", " ", tt)
+}
+
+usc_courses$clean_course_desc <- 
+  apply_context_dependency(remove_punctuation(usc_courses$course_desc))
+
+write.csv(usc_courses, "usc_courses_cleaned_with_school.csv", row.names = FALSE)
+
