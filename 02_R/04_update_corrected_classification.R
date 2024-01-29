8397ff33174c0f85e8987dbd874f40b1fbbd373c
Alison Chen <alisonchen.y@gmail.com>
2024-01-29 12:40:53 -0800

    update classification 1/29

--- 02_R/04_update_corrected_classification.R
+++ 02_R/04_update_corrected_classification.R
@@ -1,5 +1,5 @@
 # after manual fixes
-reviewed <- read.csv("USC_AY20-24_Sustainability_Courses_four_categories_1_28_24.csv")
+reviewed <- read.csv("USC_AY20-24_Sustainability_Courses_four_categories_1_29_24.csv")
 
 # together <- merge(single_rows, reviewed, all.x = TRUE)
 # together[which(is.na(together$Corrected_Sustainability_Classification)),] -> missing
@@ -10,8 +10,7 @@ prev_ge <- read.csv("ge_data_any_2_keywords.csv")
 new_ge <- merge(prev_ge, reviewed) # all exist since using most recent semester
 new_ge$sustainability_classification <- new_ge$Corrected_Sustainability_Classification
 new_ge %>%
-  select(-Automated_Sustainability_Classification, 
-         -Corrected_Sustainability_Classification) -> new_ge
+  select(-Corrected_Sustainability_Classification) -> new_ge
 write.csv(new_ge,
           "shiny_app/ge_data.csv",
           row.names = FALSE)
@@ -20,8 +19,7 @@ prev_recent <- read.csv("recent_courses_any_2_keywords.csv")
 new_recent <- merge(prev_recent, reviewed) # missing some
 new_recent$sustainability_classification <- new_recent$Corrected_Sustainability_Classification
 new_recent %>%
-  select(-Automated_Sustainability_Classification, 
-         -Corrected_Sustainability_Classification) -> new_recent
+  select(-Corrected_Sustainability_Classification) -> new_recent
 write.csv(new_recent,
           "shiny_app/recent_courses.csv",
           row.names = FALSE)
@@ -30,12 +28,11 @@ prev_full <- read.csv("usc_courses_full_any_2_keywords.csv")
 new_full <- merge(prev_full, reviewed)
 new_full$sustainability_classification <- new_full$Corrected_Sustainability_Classification
 new_full %>%
-  select(-Automated_Sustainability_Classification, 
-         -Corrected_Sustainability_Classification) -> new_full
+  select(-Corrected_Sustainability_Classification) -> new_full
 write.csv(new_full,
           "shiny_app/usc_courses_full.csv",
           row.names = FALSE)
 
 
 
-# should enrolled = 0 be shown in the dashboard?
+# should enrolled = 0 be shown in the dashboard? no
