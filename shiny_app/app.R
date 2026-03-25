27421b94ffa8a09c41c219a9089a1f36bc99609c
fy916 <fy916758473@gmail.com>
2026-03-25 05:07:56 -0700

    Update app.R
    Fixed a bug that AY27 classes are omitted

--- shiny_app/app.R
+++ shiny_app/app.R
@@ -83,7 +83,7 @@ num_top_classes <- 10
 # data for pie chart
 sustainability_related = read.csv("usc_courses_full.csv") %>%
   filter(year != "AY20") %>%
-  filter((year %in% c("AY21", "AY22", "AY23", "AY24") & total_enrolled > 0) | year %in% c("AY25") | year %in% c("AY26"))
+  filter((year %in% c("AY21", "AY22", "AY23", "AY24") & total_enrolled > 0) | year %in% c("AY25") | year %in% c("AY26") | year %in% c("AY27"))
 
 # data for download data and word cloud
 course_sdg_data <- read.csv("course_sdg_data.csv")
@@ -107,11 +107,11 @@ classes$semester <-
 
 # data for GE's
 ge_data = read.csv("ge_data.csv") %>%
-  filter((year %in% c("AY24") & total_enrolled > 0) | year %in% c("AY25") | year %in% c("AY26"))
+  filter((year %in% c("AY24") & total_enrolled > 0) | year %in% c("AY25") | year %in% c("AY26") | year %in% c("AY27"))
 
 # data for find classes by sdgs
 recent_courses = read.csv("recent_courses.csv") %>%
-  filter((year %in% c("AY24") & total_enrolled > 0) | year %in% c("AY25") | year %in% c("AY26"))
+  filter((year %in% c("AY24") & total_enrolled > 0) | year %in% c("AY25") | year %in% c("AY26") | year %in% c("AY27"))
 
 # for the ordering of GE's in dropdown
 values = c(
